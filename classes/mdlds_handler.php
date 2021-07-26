e2a642150b481ef937f8ad2e929bdb73aaed70d1
root <iseki@rsch.tuis.ac.jp>
2021-07-26 17:18:08 +0900

    first commit

---
+++ classes/mdlds_handler.php
@@ -0,0 +1,12 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+
+class mdlds_handler 
+{
+
+    public static function get_mdlds_handler($userid) 
+    {
+        return "MORE HELL World!!!";
+    }
+}
