c8d0d3ec054e78ed89356681636dadf839e6ed7d
root <iseki@rsch.tuis.ac.jp>
2021-08-19 12:34:04 +0900

    ipynb_conv.php

--- sh/ipynb_conv.php
+++ sh/ipynb_conv.php
@@ -1,22 +1,32 @@
 <?php
 /**
- ipynb_conv.php : ipnb ファイル変換  v0.9
+ ipynb_conv.php : ipnb ファイル変換  v0.9.2
          filename: と codenum: を metadata の tags の配列に追加
-                                    by Fumi.Iseki    2021 08/18
+                                    by Fumi.Iseki    2021 08/19
 */
 
 
 if ($argc<2) {
-    print('usage ... '.$argv[0]." filename\n");
+    print('usage ... '.$argv[0]." in_filename [out_filename]\n");
     exit(1);
 }
 
 //
-$filename = $argv[1];
+$in_file  = $argv[1];
+$out_file = "";
+//
+if ($argc==2) {
+    $filename = $in_file;
+}
+else if ($argc>2) {
+    $out_file = $argv[2];
+    $filename = $out_file;
+}
 
-$handle = fopen($filename, 'r');
-$contents = fread($handle, filesize($filename));
-fclose($handle);
+$fp = fopen($in_file, 'r');
+$contents = fread($fp, filesize($in_file));
+fclose($fp);
+
 $json = json_decode($contents, false);
 
 
@@ -66,5 +76,13 @@ if (is_object($json)) {
 }
 
 $text = json_encode($json, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
-print($text);
+
+if ($out_file!="") {
+    $fp = fopen($out_file, 'w');
+    fwrite($fp, $text);
+    fclose($fp);
+}
+else {
+    print($text);
+}
 
