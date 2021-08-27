9ae593d63a2fd7f3cfdb218d10171d6447e4948b
root <iseki@rsch.tuis.ac.jp>
2021-08-28 00:47:58 +0900

    version

--- version.php
+++ version.php
@@ -30,7 +30,7 @@ $plugin->requires = 2020110900;
 $plugin->component = 'mod_mdlds';
 $plugin->maturity = MATURITY_ALPHA;
 
-$plugin->release = '0.9.2';
+$plugin->release = '0.9.3';
 
-$plugin->version = 2021082702;
+$plugin->version = 2021082800;
 
