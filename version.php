04821062121344d985994d579e4f23ef1219d04a
root <iseki@rsch.tuis.ac.jp>
2021-08-29 19:23:46 +0900

    version

--- version.php
+++ version.php
@@ -26,7 +26,8 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin = new stdClass();
 
-$plugin->requires = 2020110900;
+#$plugin->requires = 2020110900;
+$plugin->requires = 2018051700;
 $plugin->component = 'mod_mdlds';
 $plugin->maturity = MATURITY_ALPHA;
 
