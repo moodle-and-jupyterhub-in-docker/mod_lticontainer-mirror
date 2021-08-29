654ee5dcd9b51d1acebcd89c800b244069a385a1
root <iseki@rsch.tuis.ac.jp>
2021-08-29 19:36:53 +0900

    lost file

---
+++ actions/lti_setting.php
@@ -0,0 +1,86 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Prints an instance of mod_mdlds.
+ *
+ * @package     mod_mdlds
+ * @copyright   2021 Fumi.Iseki <iseki@rsch.tuis.ac.jp>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+require(__DIR__.'/../../../config.php');
+require_once(__DIR__.'/../lib.php');
+
+
+$cmid = required_param('id', PARAM_INT);                                                    // コースモジュール ID
+$cm   = get_coursemodule_from_id('mdlds', $cmid, 0, false, MUST_EXIST);                     // コースモジュール
+
+$course    = $DB->get_record('course', array('id'=>$cm->course), '*', MUST_EXIST);          // コースデータ from DB
+$minstance = $DB->get_record('mdlds', array('id' => $cm->instance), '*', MUST_EXIST);       // モジュールインスタンス
+
+$mcontext = context_module::instance($cm->id);                                              // モジュールコンテキスト
+$ccontext = context_course::instance($course->id);                                          // コースコンテキスト
+
+$courseid = $course->id;
+$user_id  = $USER->id;
+
+
+///////////////////////////////////////////////////////////////////////////
+// Check
+require_login($course, true, $cm);
+//
+$mdlds_lti_edit_cap = false;
+if (has_capability('mod/mdlds:lti_edit', $mcontext)) {
+    $mdlds_lti_edit_cap = true;
+}
+
+///////////////////////////////////////////////////////////////////////////
+$urlparams = array();
+$urlparams['id'] = $cmid;
+
+$current_tab = 'lti_setting';
+$this_action = 'lti_setting';
+
+///////////////////////////////////////////////////////////////////////////
+// URL
+$base_url = new moodle_url('/mod/mdlds/actions/'.$this_action.'.php');
+$base_url->params($urlparams);
+$this_url = new moodle_url($base_url);
+
+
+///////////////////////////////////////////////////////////////////////////
+// Print the page header
+$PAGE->navbar->add(get_string('mdlds:lti_setting', 'mod_mdlds'));
+$PAGE->set_url($this_url, $urlparams);
+$PAGE->set_title(format_string($minstance->name));
+$PAGE->set_heading(format_string($course->fullname));
+$PAGE->set_context($mcontext);
+
+echo $OUTPUT->header();
+
+require(__DIR__.'/../include/tabs.php');
+require_once(__DIR__.'/../classes/lti_setting.class.php');
+
+if ($mdlds_lti_edit_cap) {
+    $lti_settingion = new LTIConnect($cmid, $courseid, $minstance);
+    $lti_settingion->set_condition();
+    $lti_settingion->execute();
+    $lti_settingion->print_page();
+}
+
+echo $OUTPUT->footer($course);
+
---
+++ html/lti_setting_table.php
@@ -0,0 +1,39 @@
+<?php
+
+function show_lti_disp_table($items)
+{
+    $table = new html_table();
+    //
+    $table->head [] = '#';
+    $table->align[] = 'center';
+    $table->size [] = '40px';
+    $table->wrap [] = 'nowrap';
+
+    $table->head [] = get_string('lti_name','mod_mdlds');
+    $table->align[] = 'center';
+    $table->size [] = '200px';
+    $table->wrap [] = 'nowrap';
+
+    $table->head [] = 'No Display';
+    $table->align[] = 'center';
+    $table->size [] = '150px';
+    $table->wrap [] = 'nowrap';
+
+    //
+    $i = 0;
+    foreach($items as $item) { 
+        $checked = '';
+        if ($item->disp==0) $checked = 'checked';
+        //
+        $url_params = array("ltiid"=>$item->id);
+        $table->data[$i][] = $i + 1;
+        $table->data[$i][] = $item->name;
+        $table->data[$i][] = '<input type="checkbox" name="nodisp['.$item->id.']" value="1" '.$checked.' />';
+        $i++;
+    }
+
+    echo '<div align="center">';
+    echo html_writer::table($table);
+    echo '</div>';
+}
+
