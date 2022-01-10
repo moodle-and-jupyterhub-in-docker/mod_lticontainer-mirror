2d42959a0ef27caae75f32a177d9c0b301f57c2d
root <iseki@rsch.tuis.ac.jp>
2022-01-11 01:13:05 +0900

    add local_define.php

---
+++ local_define.php
@@ -0,0 +1,16 @@
+<?php
+
+define('CHART_BAR_MAX_USER_NUM',  10);
+define('CHART_BAR_MAX_CODE_NUM',  15);
+define('CHART_LINE_MAX_USER_NUM', 15);
+define('CHART_LINE_MAX_INTERVAL', 1800);       // 30m
+define('CHART_REALTIME_DURING',   5400);       // 1:30m
+define('CHART_ANYTIME_DURING',    604800);     // 7days
+
+define('CHART_VIEW_SIZE',         720); 
+
+define('CHART_NULL_FILENAME',     '.none');
+define('CHART_NULL_USERNAME',     '.anyone');
+define('CHART_NULL_CODENUM',      'none');
+
+
