4b3e6cb3071dd8991cdfe8194e656f4ebf3159d3
root <iseki@rsch.tuis.ac.jp>
2022-01-11 00:52:38 +0900

    chart size

--- html/chart_view.php
+++ html/chart_view.php
@@ -15,7 +15,7 @@ function show_chart_view($args)
     // チャートを描画
     echo '<h3><strong>'.$args->chart_title.'</strong></h3>';
     echo '<table border="1" align="center" cellpadding="0" cellspacing="0">';
-    echo   '<tr><td width="820" align="center">';
+    echo   '<tr><td width="720" align="center">';
     //
     if (!empty($args->charts)) {
         foreach ($args->charts as $chart) {
