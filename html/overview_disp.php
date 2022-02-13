cb3b348b704691365a8a44aaa67b4771fbed7610
root <iseki@rsch.tuis.ac.jp>
2022-02-13 19:22:19 +0900

    over view

--- html/overview_disp.php
+++ html/overview_disp.php
@@ -2,22 +2,22 @@
 
 $disp = 'No';
 $make = 'No';
-$podm = 'No';
+$container = 'Docker';
 if ($minstance->custom_params==1) $disp = 'Yes';
 if ($minstance->make_volumes ==1) $make = 'Yes';
-if ($minstance->use_podman ==1)   $podm = 'Yes';
+if ($minstance->use_podman ==1)   $container = 'Podman';
 
 $params = array('update' => $cmid);
 $setup_url = new moodle_url('/course/modedit.php', $params);
 
 echo '<br />'; 
 echo '<h4>'; 
-print('Docker Host : <strong>'.$minstance->docker_host.'</strong><br />');
-print('Docker User : <strong>'.$minstance->docker_user.'</strong><br />');
+print('Container Host : <strong>'.$minstance->docker_host.'</strong><br />');
+print('Conrainer User : <strong>'.$minstance->docker_user.'</strong><br />');
 print('Shows LTI parameters : <strong>'.$disp.'</strong><br />');
 print('Image Name Filter : <strong>'.$minstance->imgname_fltr.'</strong><br />');
-print('Creates volumes : <strong>'.$make.'</strong><br />');
-print('Use Podman instead of Docker : <strong>'.$podm.'</strong><br />');
+print('Creates volumes  : <strong>'.$make.'</strong><br />');
+print('Container System : <strong>'.$container.'</strong><br />');
 
 if (has_capability('mod/lticontainer:db_write', $mcontext)) {
     print('<br />');
