09b114f1b49dba15c12b0221d75be087580c0d70
root <iseki@rsch.tuis.ac.jp>
2022-02-13 19:23:11 +0900

    bits

--- html/overview_disp.php
+++ html/overview_disp.php
@@ -13,10 +13,10 @@ $setup_url = new moodle_url('/course/modedit.php', $params);
 echo '<br />'; 
 echo '<h4>'; 
 print('Container Host : <strong>'.$minstance->docker_host.'</strong><br />');
-print('Conrainer User : <strong>'.$minstance->docker_user.'</strong><br />');
+print('Container User : <strong>'.$minstance->docker_user.'</strong><br />');
 print('Shows LTI parameters : <strong>'.$disp.'</strong><br />');
 print('Image Name Filter : <strong>'.$minstance->imgname_fltr.'</strong><br />');
-print('Creates volumes  : <strong>'.$make.'</strong><br />');
+print('Creates Volumes  : <strong>'.$make.'</strong><br />');
 print('Container System : <strong>'.$container.'</strong><br />');
 
 if (has_capability('mod/lticontainer:db_write', $mcontext)) {
