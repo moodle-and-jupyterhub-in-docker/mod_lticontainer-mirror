88c05873016da9a2dbada2cfea5495002d096b12
root <iseki@rsch.tuis.ac.jp>
2022-06-21 11:01:16 +0900

    v1.0.7

--- version.php
+++ version.php
@@ -30,6 +30,7 @@ $plugin->requires  = 2018051700;
 $plugin->component = 'mod_lticontainer';
 $plugin->maturity  = MATURITY_STABLE;
 
-$plugin->release   = '1.0.6';
-$plugin->version   = 2022042601;
+$plugin->release   = '1.0.7';
+$plugin->version   = 2022062100;
+//$plugin->version = 2022042601;
 
